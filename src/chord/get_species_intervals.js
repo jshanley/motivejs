84b2dddf4f6a783c56d881207d05d66eb0e652fc
John Shanley <john.michael.shanley@gmail.com>
2014-02-24 09:58:04 -0500

    update chordname to intervals

--- src/chord/get_species_intervals.js
+++ src/chord/get_species_intervals.js
@@ -16,7 +16,7 @@ module.exports = (function(){
         thirteen: ['M9','P11','M13']
     };
 
-    var species_regex = /^(maj|min|mmin|m|aug|dim|alt|sus|\-)?((?:\d+)|6\/9?)?$/;
+    var species_regex = /^(maj|min|mmin|m|aug|dim|alt|sus|\-)?((?:\d+)|(?:6\/9))?$/;
 
     return function getSpeciesIntervals(species) {
 
@@ -37,14 +37,56 @@ module.exports = (function(){
             return basic_types.sus4;
         }
 
+        var output = [];
+
         var captures = species_regex.exec(species);
 
         var prefix = captures[1] ? captures[1] : '',
             degree = captures[2] ? captures[2] : '';
 
+        switch (prefix) {
+            case '':
+                if (degree === '6/9') {
+                    output = output.concat(basic_types.maj, ['M6','M9']);
+                } else {
+                    output = output.concat(basic_types.maj, degree === '6' ? 'M6' : 'm7');
+                }
+                break;
+            case 'maj':
+                output = output.concat(basic_types.maj, degree === '6' ? 'M6' : 'M7');
+                break;
+            case 'min':
+            case 'm':
+            case '-':
+                output = output.concat(basic_types.min, degree === '6' ? 'M6' : 'm7');
+                break;
+            case 'aug':
+                output = output.concat(basic_types.aug, 'm7');
+                break;
+            case 'dim':
+                output = output.concat(basic_types.dim, 'd7');
+                break;
+            case 'mmaj':
+                output = output.concat(basic_types.min, 'M7');
+                break;
+            default:
+                break;
+        }
 
-
-
+        switch (degree) {
+            case '9':
+                output = output.concat(extensions.nine);
+                break;
+            case '11':
+                output = output.concat(extensions.eleven);
+                break;
+            case '13':
+                output = output.concat(extensions.thirteen);
+                break;
+            default:
+                break;
+        }
+        return output;
     };
 
 })();
