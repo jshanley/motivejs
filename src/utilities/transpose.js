74316c0937b8d11477dbacd3933562e2581c0142
johnshanley <john.michael.shanley@gmail.com>
2014-03-08 10:30:05 -0500

    break up long lines

--- src/utilities/transpose.js
+++ src/utilities/transpose.js
@@ -19,14 +19,17 @@ function transpose(note_name, direction, interval) {
 
     var factor = direction === 'up' ? 1 : -1;
 
-    var new_note_name = fifths.atIndex(fifths.indexOf(parsed_n.step + parsed_n.accidental) + (factor * intervals.indexOf(interval)));
-    
+    var new_note_name = fifths.atIndex(
+      fifths.indexOf(parsed_n.step + parsed_n.accidental) +
+      (factor * intervals.indexOf(interval))
+    );
+
     // check if octave adjustment is needed
     if (parsed_n.octave === null) {
         return new_note_name;
     }
-    // octave adjustment
     
+    // octave adjustment
     var new_octave = parsed_n.octave + (factor * Math.floor(parsed_i.size / 8));
     var normalized_steps = parsed_i.size > 7 ? (parsed_i.size % 7) - 1 : parsed_i.size - 1;
     if ((steps.indexOf(parsed_n.step) + normalized_steps) >= 7) {
@@ -36,4 +39,3 @@ function transpose(note_name, direction, interval) {
 }
 
 module.exports = transpose;
-
