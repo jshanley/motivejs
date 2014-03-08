fec6281d53c1a308dd1f71345b3aa40dde0ab3ea
johnshanley <john.michael.shanley@gmail.com>
2014-03-08 11:44:23 -0500

    basic palette functionality implemented

--- src/palette/palette.js
+++ src/palette/palette.js
@@ -1,13 +1,27 @@
 var note = require('../note/note');
 
-function Palette() {
-
+function Palette(item) {
+	this.notes = [];
+	if (typeof item !== 'undefined') {
+		this.add(item);
+	}
 }
 Palette.prototype.add = function(item) {
+	for (var i = 0; i < item.notes.length; i++) {
+		var inThis = false;
+		for (var t = 0; t < this.notes.length; t++) {
+			if (this.notes[t].isEquivalent(item.notes[i])) {
+				inThis = true;
+				break;
+			}
+		}
+		if (!inThis) {
+			this.notes.push(item.notes[i]);
+		}
+	}
+};
 
-}
 
-
-module.exports = function() {
-	return new Palette(arguments);
-}
+module.exports = function(item) {
+	return new Palette(item);
+};
