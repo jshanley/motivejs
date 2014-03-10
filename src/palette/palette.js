302b9a016346f7c156713b7e2ed836a62fcf8e94
johnshanley <john.michael.shanley@gmail.com>
2014-03-10 15:56:17 -0400

    allow single notes to be added to palette

--- src/palette/palette.js
+++ src/palette/palette.js
@@ -6,17 +6,24 @@ function Palette(item) {
   }
 }
 Palette.prototype.add = function(item) {
-  for (var i = 0; i < item.notes.length; i++) {
-    var inThis = false;
-    for (var t = 0; t < this.notes.length; t++) {
-      if (this.notes[t].isEquivalent(item.notes[i])) {
-        inThis = true;
-        break;
-      }
+  if (item.type === 'note') {
+    this.notes.push(item);
+    return;
+  }
+  if (item.type === 'chord') {
+    for (var i = 0; i < item.notes.length; i++) {
+     var inThis = false;
+     for (var t = 0; t < this.notes.length; t++) {
+       if (this.notes[t].isEquivalent(item.notes[i])) {
+         inThis = true;
+         break;
+       }
+     }
+     if (!inThis) {
+       this.notes.push(item.notes[i]);
+     }
     }
-    if (!inThis) {
-      this.notes.push(item.notes[i]);
-    }
+    return;
   }
 };
 
