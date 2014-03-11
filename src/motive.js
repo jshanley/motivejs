2864eec89e6a276c6aac6284599f4045eb6de311
johnshanley <john.michael.shanley@gmail.com>
2014-03-11 11:14:10 -0400

    add semver to motive object

--- src/motive.js
+++ src/motive.js
@@ -1,5 +1,6 @@
 // this will be the global object
 module.exports = {
+  version: require('../package.json').version,
   note: require('./note/note'),
   chord: require('./chord/jazz'),
   interval: require('./interval/interval'),
