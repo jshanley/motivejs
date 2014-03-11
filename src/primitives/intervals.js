e09fd9a52e4203f18f71354653d749029d993027
johnshanley <john.michael.shanley@gmail.com>
2014-03-11 10:50:04 -0400

    comment interval logic

--- src/primitives/intervals.js
+++ src/primitives/intervals.js
@@ -2,6 +2,12 @@ var Circle   = require('../math/circle'),
     modulo   = require('../math/modulo'),
     validate = require('../regex/validation/interval_name');
 
+// these values represent the size of intervals arranged by fifths.
+// Given 4, each value is value[i] = mod7(value[i-1] + 4) with
+//   the exception that zero is avoided by setting mod7(7) = 7
+
+// TODO starting with 4 seems arbitrary, can this be adjusted to start with 1
+//   as index 0, and put 4 on the end as index -1
 var intervals = new Circle([4,1,5,2,6,3,7]);
 intervals.indexOf = function(interval_name) {
 
