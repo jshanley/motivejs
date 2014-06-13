42a3403f85c77f9b554e040f425412120c5044fd
johnshanley <john.michael.shanley@gmail.com>
2014-06-13 13:16:57 -0400

    added a few more comments

--- src/key/key.js
+++ src/key/key.js
@@ -1,14 +1,16 @@
 var Key = function(keyInput) {
+  // run input through validation
   var parsed = regex.validate.keyName(keyInput).parse();
   if (!parsed) {
     throw new Error('Invalid key name: ' + keyInput.toString());
   }
+  // assign mode based on the parsed input's quality
   if (/[a-g]/.test(parsed.step) || parsed.quality === 'minor' || parsed.quality === 'm') {
     this.mode = 'minor';
   } else {
     this.mode = 'major';
   }
-  // now that we have the mode, enforce uppercase
+  // now that we have the mode, enforce uppercase for root note
   parsed.step = parsed.step.toUpperCase();
   // get fifths for major key
   this.fifths = circles.fifths.indexOf(parsed.step + parsed.accidental);
@@ -20,4 +22,4 @@ var Key = function(keyInput) {
     this.name = parsed.step + parsed.accidental + ' major';
   }
   return this;
-}
+};
