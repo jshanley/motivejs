6d02a2621f044cebf1b227b0ddc8317389afa54e
John Shanley <john.michael.shanley@gmail.com>
2014-11-04 12:09:15 -0500

    update tests to reflect changes in #5

--- test/chord_test.js
+++ test/chord_test.js
@@ -4,7 +4,7 @@ exports['chord'] = function(test) {
   test.expect(4);
   test.ok(chord);
   test.equal(chord('Dm7').root.name, 'D');
-  test.equal(chord('A7#9').notes.length, 5);
+  test.equal(chord('A7#9').notes.contains('C#'), true);
   test.equal(chord('F#/A#').bass.name, 'A#');
   test.done();
 };
