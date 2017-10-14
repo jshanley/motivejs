b382bfbd03bed8c28c9f0b99037f417a0a5071a5
John Shanley <john.michael.shanley@gmail.com>
2017-10-14 10:52:58 -0400

    Use rollup for bundling

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * MotiveJS
- * https://github.com/jshanley/motivejs/
- * Copyright (c) 2014 John Shanley
- * Licensed under the MIT license.
- */
-
-exports = module.exports = require('./motive.min').default;
---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import pkg from './package.json';
+
+
+export default {
+  input: 'tmp/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
+    { file: pkg.browser, format: 'umd', name: 'motive' },
+  ],
+  sourcemap: true
+}
